a193e223de4e0c15985716b94fd4f3fc0c1b744a
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-11-21 20:40:17 -0500

    Edited Hello Frenemy IO

---
+++ Unit-2.6-Hello_Frenemy_IO/starter_code.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(questionText, resolve);
+  });
+}
+
+let enemyList = ['darth vader', 'voldemort', 'palatine', 'lex luthor'];
+
+async function respond() {
+  // your code here
+  // remember to use `await`
+}
+respond();

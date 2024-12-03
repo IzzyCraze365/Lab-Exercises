c68752eed4d8ac62cb217ae2d4734f834b3ef6d7
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-03 18:47:20 -0500

    workiing on Walkway Objects

--- Unit-3.6-Walkway-Objects/index.js
+++ Unit-3.6-Walkway-Objects/index.js
@@ -1,37 +1,40 @@
 class Location {
-    // your code here
-  
+  // your code here
+  constructor(name, description) {
+    (this.name = name), (this.description = description);
   }
-  
-  let home = new Location(/* your code here */);
-  let sidewalk = new Location(/* your code here */);
-  let store = new Location(/* your code here */);
-  
-  let locationCurrent = "home";
-  
-  let locationLookUp = {
-    // your code here
-  };
-  
-  let locationStates = {
-    // your code here
-  };
-  
-  
-  function moveLocation(newLocation) {
-    // your code here
-  }
-  
-  moveLocation("sidewalk");
-  // Prints 'You are on the sidewalk.'
-  moveLocation("store");
-  // Prints 'You are in the store.'
-  moveLocation("home");
-  // Prints 'You cannot go from store to home.'
-  moveLocation("sidewalk");
-  // Prints 'You are on the sidewalk.'
-  moveLocation("home");
-  // Prints 'You are at your house.'
-  moveLocation("park");
-  // Prints 'You cannot go from sidewalk to park.'
-  
+}
+
+let home = new Location(/* your code here */`home`, `You are Home`);
+let sidewalk = new Location(/* your code here */"sidewalk","You are on the Sidewalk");
+let store = new Location(/* your code here */'sidewalk','You are on the Sidewalk');
+
+let locationCurrent = "home"; //! This represents our initial state
+
+let locationLookUp = {
+  // the key is a string that matches the EXACT names of our Objects
+  "home": home,
+  "sidewalk":sidewalk,
+  "store":store
+};
+
+let locationStates = {
+  // your code here
+};
+
+function moveLocation(newLocation) {
+  // your code here
+}
+
+moveLocation("sidewalk");
+// Prints 'You are on the sidewalk.'
+moveLocation("store");
+// Prints 'You are in the store.'
+moveLocation("home");
+// Prints 'You cannot go from store to home.'
+moveLocation("sidewalk");
+// Prints 'You are on the sidewalk.'
+moveLocation("home");
+// Prints 'You are at your house.'
+moveLocation("park");
+// Prints 'You cannot go from sidewalk to park.'

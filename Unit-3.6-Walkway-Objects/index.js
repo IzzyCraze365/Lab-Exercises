3fe989710561ad44765e2bce6095a2372c7393c9
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-03 19:16:14 -0500

    Completed Base Walkway Objects

--- Unit-3.6-Walkway-Objects/index.js
+++ Unit-3.6-Walkway-Objects/index.js
@@ -5,25 +5,48 @@ class Location {
   }
 }
 
-let home = new Location(/* your code here */`home`, `You are Home`);
-let sidewalk = new Location(/* your code here */"sidewalk","You are on the Sidewalk");
-let store = new Location(/* your code here */'sidewalk','You are on the Sidewalk');
+let home = new Location(/* your code here */ `home`, `You are Home`);
+let sidewalk = new Location(
+  /* your code here */ "sidewalk",
+  "You are on the Sidewalk"
+);
+let store = new Location(
+  /* your code here */ "sidewalk",
+  "You are on the Sidewalk"
+);
 
 let locationCurrent = "home"; //! This represents our initial state
 
 let locationLookUp = {
   // the key is a string that matches the EXACT names of our Objects
-  "home": home,
-  "sidewalk":sidewalk,
-  "store":store
+  home: home,
+  sidewalk: sidewalk,
+  store: store,
 };
 
 let locationStates = {
-  // your code here
+  // this determines what locations are adjacent to the current location
+  home: ["sidewalk"], // you can move from home to the sidewalk
+  sidewalk: ["home", "store"], // you can move from the sidewalk to the store or home
+  store: ["sidewalk"], // you can move from the store to the sidewalk
 };
 
+console.log(locationLookUp[locationCurrent].description); //! this is the dynamic console.log that will change based off of where we are
+// This console log is the crux of the entire project and will update the state that you are in.
+
 function moveLocation(newLocation) {
-  // your code here
+  console.log("You are trying to move to the", newLocation + "...");
+  if (locationStates[locationCurrent].includes(newLocation)) {
+    console.log(
+      `You have moved from the ${locationLookUp[locationCurrent].name} to the ${newLocation}.`
+    );
+    locationCurrent = newLocation;
+    console.log(locationLookUp[locationCurrent].description);
+  } else {
+    console.log(
+      `You cannot move from the ${locationCurrent} to the ${newLocation}.  Who do you think you are the Koolaide Man?!`
+    );
+  }
 }
 
 moveLocation("sidewalk");

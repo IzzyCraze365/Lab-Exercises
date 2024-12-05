a3d2735ed9fcf5ab9236721a64b58a674a6260e5
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-04 20:32:47 -0500

    Completed Fibonacci Stack

--- Unit-3.1-Fibonacci-Stack/index.js
+++ Unit-3.1-Fibonacci-Stack/index.js
@@ -1,12 +1,29 @@
 // Create the `series` variable
 var series = [0, 1];
+let finalNumber = 0;
+let penultimateNumber = 0;
+
+let maxLength = 10; // This can be adjusted to see how many numbers we want in our Fibonacci squeence.
 
 /* your check in the while loop */
-while () {
-  // your code here
+function fibonacci(maxLength) {
+  while (series.length < maxLength) {
+    // your code here
+    finalNumber = series.pop();
+    //console.log("Final Number", finalNumber); //! TEST
+    penultimateNumber = series.pop();
+    //console.log("Second to Last Number", penultimateNumber); //! TEST
+    let sum = finalNumber + penultimateNumber;
+    series.push(penultimateNumber);
+    series.push(finalNumber);
+    series.push(sum);
+    //console.log("Fibonacci sequence", series); //! TEST
+  }
+  console.log("Fibonacci sequence", series);
 }
-
 // Print the Fibonacci sequence to the console
 console.log(series);
 // the first ten numbers should be
 // 0 1 1 2 3 5 8 13 21 34
+
+fibonacci(50); // Change the parameter to get different length of Fibonacci Stack

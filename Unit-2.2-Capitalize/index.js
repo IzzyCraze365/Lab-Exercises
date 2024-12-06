14fab1763839519df056d39ba3b3a060cf5b76b4
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-06 12:54:08 -0500

    Completed Capitalize

--- Unit-2.2-Capitalize/index.js
+++ Unit-2.2-Capitalize/index.js
@@ -1,5 +1,11 @@
 function capitalize(someWord) {
-    // your code here
+  // your code here
+  let firstLetter = someWord.charAt(0);
+  let restOfWord = someWord.slice(1);
+  firstLetter = firstLetter.toUpperCase();
+  restOfWord = restOfWord.toLowerCase();
+  let fullWord = firstLetter + restOfWord;
+  return fullWord;
 }
 
 console.log(capitalize("tomato")); // should print 'Tomato'
@@ -7,11 +13,19 @@ console.log(capitalize("BACON")); // should print 'Bacon'
 console.log(capitalize("LeTTuCe")); // should print 'Lettuce'
 console.log(capitalize("mayonnAISE")); // should print 'Mayonnaise'
 
-
-//! Capitalize the given input
+//! Alternative ways to do this that work for full sentences
+// Split up the strings by the space " "
 function capitalizeInput(myString) {
-    return (wordsCapitolized = myString
-      .split(" ")
-      .map((word) => capitalizeFirstLetter(word))
-      .join(" "));
-  }
+  return (wordsCapitolized = myString
+    .split(" ")
+    .map((word) => capitalizeFirstLetter(word))
+    .join(" "));
+}
+
+// Capitalize the given input
+function capitalizeFirstLetter(word) {
+  word = word.trim();
+  let firstLetter1 = word.charAt(0).toUpperCase();
+  let restOfWord1 = word.slice(1).toLowerCase();
+  return firstLetter1 + restOfWord1;
+}

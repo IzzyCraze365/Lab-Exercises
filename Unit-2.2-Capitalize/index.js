230d399d60d9bbb9ed8ddd42a61924890532c8e6
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-06 12:48:11 -0500

    Started Capitalize

--- Unit-2.2-Capitalize/index.js
+++ Unit-2.2-Capitalize/index.js
@@ -6,3 +6,12 @@ console.log(capitalize("tomato")); // should print 'Tomato'
 console.log(capitalize("BACON")); // should print 'Bacon'
 console.log(capitalize("LeTTuCe")); // should print 'Lettuce'
 console.log(capitalize("mayonnAISE")); // should print 'Mayonnaise'
+
+
+//! Capitalize the given input
+function capitalizeInput(myString) {
+    return (wordsCapitolized = myString
+      .split(" ")
+      .map((word) => capitalizeFirstLetter(word))
+      .join(" "));
+  }

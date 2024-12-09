356264db50fd51e35b1aa7c3a3cae57abe197c4d
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-09 18:45:46 -0500

    Completed Hello Frenemy

--- Unit-2.5-Hello_Frenemy/index.js
+++ Unit-2.5-Hello_Frenemy/index.js
@@ -1,3 +1,32 @@
 function greeter(name) {
   // Your code goes here
+  let nameCheck = capitalizeInput(name);
+  if(name.toLowerCase() === "enemy"){
+    console.log("Go away,", nameCheck+"!");
+  }else{
+    console.log(`Hello, ${nameCheck}.`)
+  }
+}
+
+greeter("enemy"); // Prints "Go away, Enemy!"
+greeter("enEmy"); // Prints "Go away, Enemy!"
+greeter("FrIeNd"); // Prints "Hello, Friend."
+greeter("brUCE wAyNE"); // Prints "Hello, Bruce Wayne."
+
+
+//! The following functions were taken from the Capitalize Assignment
+// Split up the strings by the space " "
+function capitalizeInput(myString) {
+  return (wordsCapitolized = myString
+    .split(" ")
+    .map((word) => capitalizeFirstLetter(word))
+    .join(" "));
+}
+
+// Capitalize the given input
+function capitalizeFirstLetter(word) {
+  word = word.trim();
+  let firstLetter1 = word.charAt(0).toUpperCase();
+  let restOfWord1 = word.slice(1).toLowerCase();
+  return firstLetter1 + restOfWord1;
 }

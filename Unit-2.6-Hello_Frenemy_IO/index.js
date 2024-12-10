f4a040a0a8b3f67e5f93861c2c585e8074962d25
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-09 19:04:04 -0500

    Startedd Hello Frenemy I/O

--- Unit-2.6-Hello_Frenemy_IO/index.js
+++ Unit-2.6-Hello_Frenemy_IO/index.js
@@ -15,5 +15,36 @@ let enemyList = ['darth vader', 'voldemort', 'palatine', 'lex luthor'];
 async function respond() {
   // your code here
   // remember to use `await`
+const greeting = `What is your name?\n`;
+  let guestName = await ask(greeting);
+  let nameChecker = capitalizeInput(guestName);
+  if(guestName.toLowerCase() === "darth vader"){
+    console.log("Noooooo! That's impossible!");
+  }
+  else if(enemyList.includes(guestName.toLowerCase())){
+    console.log("Go away,", nameChecker+"!");
+  }else{
+    console.log(`Hello, ${nameChecker}.`)
+  }
+
+
 }
 respond();
+
+
+//! The following functions were taken from the Capitalize Assignment
+// Split up the strings by the space " "
+function capitalizeInput(myString) {
+  return (wordsCapitolized = myString
+    .split(" ")
+    .map((word) => capitalizeFirstLetter(word))
+    .join(" "));
+}
+
+// Capitalize the given input
+function capitalizeFirstLetter(word) {
+  word = word.trim();
+  let firstLetter1 = word.charAt(0).toUpperCase();
+  let restOfWord1 = word.slice(1).toLowerCase();
+  return firstLetter1 + restOfWord1;
+}

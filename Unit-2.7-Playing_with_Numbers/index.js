7c45809ccfe5b425dbf10617ab7700e0b94fa853
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-09 19:18:52 -0500

    Completed Playing with Numbers

--- Unit-2.7-Playing_with_Numbers/index.js
+++ Unit-2.7-Playing_with_Numbers/index.js
@@ -9,3 +9,17 @@ Try the following and console log the result:
 
 Add your code below
 */
+
+//* If you use a "+" to string concatinate all the numbers are treated as a string.  If you use a comma "," the second half is treated as a number and the first half is treated as a string
+console.log("5+3 =", 5 + 3); // Prints "5+3 = 8"
+console.log("5*3 =" + 5 * 3); // Prints "5*3 =15"
+console.log("15/3 =" + 15 / 3); // Prints "15/3 =5"
+console.log("5**3 =", 5 ** 3); // Prints "5**3 = 125"
+console.log(`(15*4)**3/30+7 = ${(15 * 4) ** 3 / 30 + 7}`); // Prints "(15*4)**3/30+7 = 7207"
+
+console.log(0.1 + 0.2); // Prints "0.30000000000000004 "
+console.log(2 ** 53); // Prints "9007199254740992"
+console.log(2 ** 53 + 1); // Prints "9007199254740992   "
+console.log(Infinity - Infinity); // Prints "NaN"
+
+//! There are limits to how much JS can calculate

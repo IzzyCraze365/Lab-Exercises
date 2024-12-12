9bd04d44404cbc4006629a1feff849ac0619b577
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-12 18:36:03 -0500

    Worked on Mission Countdown

--- Unit-4.1-Mission-Countdown/script.js
+++ Unit-4.1-Mission-Countdown/script.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// Go Fishing, get the IDs from the HTML linked up
+let startTimeout = getElementById(start-timeout);
+let timeoutDisplay = getElementById(timeout-display);
+let startInterval = getElementById(start-interval);
+let intervalDisplay = getElementById(interval-display);
+
+// Event Listeners
+startTimeout.addEventListener();
+startInterval.addEventListener();

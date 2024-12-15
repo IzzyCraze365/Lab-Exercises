17d610ee1dc9a94d08d094f982f25c9bbe586ef9
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-12-15 03:13:06 -0500

    Mission Countdown - Fixed Get Element by ID, added to function

--- Unit-4.1-Mission-Countdown/script.js
+++ Unit-4.1-Mission-Countdown/script.js
@@ -1,15 +1,16 @@
 // Your code goes here
 
 // Go Fishing, get the IDs from the HTML linked up
-let startTimeout = getElementById(start-timeout);
-let timeoutDisplay = getElementById(timeout-display);
-let startInterval = getElementById(start-interval);
-let intervalDisplay = getElementById(interval-display);
+let startTimeout = document.getElementById("start-timeout");
+let timeoutDisplay = document.getElementById("timeout-display");
+let startInterval = document.getElementById("start-interval");
+let intervalDisplay = document.getElementById("interval-display");
 
 // Event Listeners
 startTimeout.addEventListener("click", startingTimeout);
 startInterval.addEventListener("click", functionNameHere);
 
-function startingTimeout(){
-
+function startingTimeout() {
+  console.log("Start Timeout Clicked");
+  timeoutDisplay.textContent = "Getting Ready";
 }

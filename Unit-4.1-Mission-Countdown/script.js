a0a3612044e1fa196d16e2b2603b2ede09a69ea7
John Isabella III <johnaisabella3@gmail.com>
2024-12-17 17:33:37 -0500

    Mission Countdown - adding setInterval

--- Unit-4.1-Mission-Countdown/script.js
+++ Unit-4.1-Mission-Countdown/script.js
@@ -13,11 +13,24 @@ startInterval.addEventListener("click", functionNameHere);
 function startingTimeout() {
   console.log("Start Timeout Clicked");
   timeoutDisplay.textContent = "Getting Ready";
-  setTimeout(countDown,10,10);
+  // This is how setTimeout works
+  /* setTimeout(functionRef, delay, param1, param2, …, paramN) */
+  setTimeout(countDown, 5000); // 5 seconds, timer in miliseconds
   timeoutDisplay.textContent = "READY!";
-
 }
 
-function countDown(time){
-
+function countDown() {
+  let count = 5;
+  // This is how setInterval works
+  /* setInterval(func, delay, arg1, arg2, ...  argN) */
+  let timer = setInterval(countingDown, 1000, count);
 }
+  function countingDown(count){
+    if(count < 0){
+      intervalDisplay.textContent = "GO!";
+      clearInterval(timer);
+     }
+        console.log('Count',count);
+        ((intervalDisplay.textContent = i), 1000);
+      }
+  }

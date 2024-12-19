e0ea5018ef35d9aabc89ebd180444eabacd6276b
John Isabella III <johnaisabella3@gmail.com>
2024-12-19 18:47:45 -0500

    Reading Form Data - installations

--- Unit-5.3-reading-form-data/index.js
+++ Unit-5.3-reading-form-data/index.js
@@ -1,5 +1,12 @@
+//* don't forget to do the following commands on the terminal
+
+//! What Tyler Installs
 // npm init -y
-// start with npm i express
+// npm install express
+// touch server.js
+// echo $null >> server.js
+
+
 
 const express = require('express');
 const app = express();

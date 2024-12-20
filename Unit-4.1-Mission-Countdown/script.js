e8b08058d6869e7329943c4612a4c58dd1ef7c6e
John Isabella III <johnaisabella3@gmail.com>
2024-12-20 07:53:01 -0500

    Mission Countdown - startingTimeout function fixed so it says 'Getting Ready' and 'Ready' correctly

--- Unit-4.1-Mission-Countdown/script.js
+++ Unit-4.1-Mission-Countdown/script.js
@@ -7,16 +7,21 @@ let startInterval = document.getElementById("start-interval");
 let intervalDisplay = document.getElementById("interval-display");
 
 // Event Listeners
-startTimeout.addEventListener("click", startingTimeout);
-startInterval.addEventListener("click", functionNameHere);
+startTimeout.addEventListener("click", () => {
+  startingTimeout(); //This states the "Getting Ready" => "Ready"
+});
+startInterval.addEventListener("click", () => {
+  countDown(); // This starts our countdown
+}); 
 
 function startingTimeout() {
   console.log("Start Timeout Clicked");
-  timeoutDisplay.textContent = "Getting Ready";
+  timeoutDisplay.textContent = "Getting Ready...";
   // This is how setTimeout works
   /* setTimeout(functionRef, delay, param1, param2, …, paramN) */
-  setTimeout(countDown, 5000); // 5 seconds, timer in miliseconds
-  timeoutDisplay.textContent = "READY!";
+  setTimeout(() => {
+    timeoutDisplay.textContent = "READY!";
+  }, 5000); // 5 seconds, timer in miliseconds
 }
 
 function countDown() {
@@ -24,10 +29,11 @@ function countDown() {
   let count = 5;
   // This is how setInterval works
   /* setInterval(func, delay, arg1, arg2, ...  argN) */
-  let timer = setInterval(countingDown, 1000, count);
+  let timer = setInterval(()=>countingDown(count), 1000, count);
 }
 function countingDown(count) {
-  console.log("insdie countingDown");
+  console.log("inside countingDown");
+  intervalDisplay.textContent = count;
   if (count < 0) {
     intervalDisplay.textContent = "GO!";
     clearInterval(timer);

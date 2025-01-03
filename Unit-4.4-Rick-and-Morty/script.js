daeeb41658b564ba65e1c86e5cfde3f6b43dae94
John Isabella III <johnaisabella3@gmail.com>
2025-01-03 12:22:19 -0500

    Rick and Morty - query selector added

--- Unit-4.4-Rick-and-Morty/script.js
+++ Unit-4.4-Rick-and-Morty/script.js
@@ -1,8 +1,12 @@
+let image1 = document.querySelector("#firstImage");
+let image2 = document.querySelector("#secondImage");
+
 async function render(fi, si) {
     let response = await fetch(`https://rickandmortyapi.com/api/character/${fi},${si}`)
     let data = await response.json()
     console.log(data)
     // Your code goes here
+
     //! Something will need to happen.
 }
 

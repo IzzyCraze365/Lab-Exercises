46fca53ff29fe6a344bc6afe660bf97cc3e1e77c
John <128085030+IzzyCraze365@users.noreply.github.com>
2025-02-04 00:18:15 -0500

    Combining Data Types solution folder added

---
+++ Unit-2.1-Combining_Data_Types/solution/index.js
@@ -0,0 +1,36 @@
+console.log(typeof 42);
+console.log(typeof 1 + 7);
+
+console.log("-------Addition--------");
+console.log("cheese" + 42);
+const numPlusStr = "4" + 2;
+console.log(numPlusStr);
+console.log(typeof numPlusStr);
+console.log();
+const strPlusBool = "pie" + true;
+console.log(strPlusBool);
+console.log(typeof strPlusBool);
+console.log(false + 8);
+console.log(true + 8);
+console.log();
+console.log(7 + null);
+console.log(7 + undefined);
+console.log("cat" + null);
+console.log("cat" + undefined);
+
+console.log("-------Subtraction--------");
+console.log(7 - "one");
+console.log(17 - true);
+console.log(17 - false);
+
+console.log("-------Other--------");
+console.log("cheese" / 42);
+console.log("cheese" * 42);
+console.log("cheese" % 42);
+console.log();
+console.log(true / 42);
+console.log(false / 42);
+console.log(true * 42);
+console.log(false * 42);
+console.log(true % 42);
+console.log(false % 42);
